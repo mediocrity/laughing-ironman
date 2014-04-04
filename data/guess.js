81b73801295aaa7d7e05bf7664a269c3230c6fd4
mediocrity <kristiansson.tobias@gmail.com>
2014-04-04 23:04:25 +0200

    Remove all functionality except for guessing.

--- data/guess.js
+++ data/guess.js
@@ -1,13 +1,14 @@
 
 function guess_search_bar () {
     var priority = [
-        "*[type=search]",
-        "*[type=text]",
-        '*[name=q]'
+        "input[type=search]",
+        'input[name=q]',
+        "input[type=text]"
+
     ];
 
     for (var i = 0; i < priority.length; i++) {
-        var selected = $(priority[i]+':visible');
+        var selected = $(priority[i] + ':visible');
         if (selected.length > 0) {
             return selected.first();
         }
@@ -15,9 +16,7 @@ function guess_search_bar () {
 }
 
 self.port.on('make_guess', function() {
-    var guess = guess_search_bar();
-    if (guess) {
-        guess.focus();
-    }
-
+    var guess = guess_search_bar()
+    console.log(guess);
+    guess.focus();
 });
--- data/set-focus.js
+++
@@ -1,5 +0,0 @@
-
-self.port.on('set_focus', function(path) {
-    console.log('Getting path: ' + path);
-    $(path).focus();
-});
